0a383208c1baada7fc008773164e402cdd7a4230
Isabella Riquetti <riquetti.isabella@gmail.com>
2021-01-08 01:19:42 -0300

    BinarySearch 1

---
+++ Algoritmos/Algoritmos/BinarySearch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritmos
+{
+    public class BinarySearch
+    {
+        #region Page 27 example
+        private readonly List<int> list;
+        public BinarySearch(List<int> _list)
+        {
+            list = _list;
+        }
+
+        public BinarySearch(int lenght)
+        {
+            list = new List<int>();
+            for (int i = 0; i < lenght; i++)
+                list.Add(i*5);
+        }
+
+        public int? Execute(int item)
+        {
+            int low = 0;
+            int high = list.Count - 1;
+            int middle = 0;
+
+            while (low <= high)
+            {
+                middle = (low + high) / 2;
+                int guess = list[middle];
+
+                if (guess == item)
+                    return middle;
+
+                if (guess > item)
+                    high = middle - 1;
+
+                if (guess < item)
+                    low = middle + 1;
+            }
+
+            return null;
+        }
+        #endregion
+
+        #region Page 28 exercises
+
+        public double Section1Exercise1()
+        {
+            //log.2. 128 = 7
+            return Math.Log2(128);
+        }
+
+        public double Section1Exercise2()
+        {
+            //log.2. 256 = 8
+            return Math.Log2(256);
+        }
+
+        #endregion
+    }
+}
--- Algoritmos/Algoritmos/Program.cs
+++ Algoritmos/Algoritmos/Program.cs
@@ -7,6 +7,16 @@ namespace Algoritmos
     {
         static void Main(string[] args)
         {
+            var list = new List<int>() { 1, 3, 5, 7, 9 };
+            var binarySearch = new BinarySearch(list);
+            var result1 = binarySearch.Execute(3);
+            Console.WriteLine("The example code test 1 result was: " + (result1?.ToString() ?? "NULL"));
+            var result2 = binarySearch.Execute(-1);
+            Console.WriteLine("The example code test 2 result was: " + (result2?.ToString() ?? "NULL"));
+
+            Console.WriteLine("The exercise 1.1 answer is: " + binarySearch.Section1Exercise1());
+            Console.WriteLine("The exercise 1.2 answer is: " + binarySearch.Section1Exercise2());
+
             Console.ReadKey();
         }
     }
