d432118f33f7a9a1658b597d769437b2f6b96c30
Isabella Riquetti <riquetti.isabella@gmail.com>
2021-01-08 01:19:16 -0300

    Project creation

---
+++ Algoritmos/Algoritmos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algoritmos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
