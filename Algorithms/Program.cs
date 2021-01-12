4d787d290723217787406f850517f05e895be6b7
Isabella Riquetti <riquetti.isabella@gmail.com>
2021-01-12 00:45:45 -0300

    SelectionSort

---
+++ Algorithms/Program.cs
@@ -0,0 +1,24 @@
+﻿using Algorithms;
+using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var binarySearch = new SelectionSort(10, 0, 100);
+            var result1 = binarySearch.Execute();
+            Console.WriteLine("Resultado 1: ");
+            foreach (var r1 in result1)
+                Console.Write(r1 + "\t");
+
+            //No way
+            var binarySearch2 = new SelectionSort(99999999, 0, 99999999);
+            var result2 = binarySearch2.Execute();
+            Console.WriteLine(result2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorithms/SelectionSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    public class SelectionSort
+    {
+        private readonly int[] list;
+
+        public SelectionSort(int[] _list)
+        {
+            list = _list;
+        }
+
+        public SelectionSort(int lenght, int min, int max)
+        {
+            var random = new Random();
+            list = new int[lenght];
+            for (int i = 0; i < lenght; i++)
+                list[i] = random.Next(min, max);
+        }
+
+        #region Page 54 modified example to fit C#
+
+        public int[] Execute()
+        {
+            var listOrdered = list;
+            for (int i = 0; i < listOrdered.Length; i++)
+            {
+                int min = i;
+                for (int j = i+1; j < listOrdered.Length; j++)
+                {
+                    if (listOrdered[j] < listOrdered[min])
+                        min = j;
+                }
+
+                var aux = listOrdered[min];
+                listOrdered[min] = listOrdered[i];
+                listOrdered[i] = aux;
+            }
+
+            return listOrdered;
+        }
+
+        #endregion
+    }
+}
